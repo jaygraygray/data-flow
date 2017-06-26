868dab14320e29217084503d1f9f7492d1bd4e73
Jeremy Gray <jeremygray001@gmail.com>
2017-06-26 10:12:38 -0600

    Updated Router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import registerServiceWorker from './registerServiceWorker';
-import { HashHistory, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <HashHistory>
+  <HashRouter>
     <App />
-  </HashHistory>, 
+  </HashRouter>, 
 document.getElementById('root'));
 registerServiceWorker();
